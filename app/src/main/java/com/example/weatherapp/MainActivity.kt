0afb3e439477bc666f081a50c33ba3b1932cef38
Georgios Rounis <georg.rounis@gmail.com>
2023-09-15 11:28:24 +0200

    1st version of the app

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -20,7 +19,7 @@ import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
-    val city: String = "London"
+    val city: String = "Athens"
     val API: String = "1497dd20e8bdb334b5a1f8a77050bc76"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,19 +31,46 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.errorText).visibility = View.GONE
         }, doInBackground = {
             var response: String?
+            var url = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$API"
             try{
-            response = URL("https://api.openweathermap.org/data/2.5/weather?q=city&units=metric&appid=API").readText(Charsets.UTF_8)
+                response = URL(url).readText(Charsets.UTF_8)
             }
             catch (e:Exception) {
                 response = null
             }
-        }, onPostExecute = {
+            response
+        }, onPostExecute = { response->
             try {
-                val jsonObj = JSONObject()
-                //val main = jsonObj.getJSONObject("main")
-                //val sys = jsonObj.getJSONObject("sys")
-                //val wind = jsonObj.getJSONObject("wind")
-                //val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val jsonObj = JSONObject(response )
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updatedAt: Long = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp = main.getString("temp") + "°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
+                val humidity = main.getString("humidity")
+                val sunrise:Long = sys.getLong("sunrise")
+                val sunset:Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val location = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                findViewById<TextView>(R.id.location).text = location
+                findViewById<TextView>(R.id.updated_at).text = updatedAtText
+                findViewById<TextView>(R.id.status).text = weatherDescription
+                findViewById<TextView>(R.id.temperature).text = temp
+                findViewById<TextView>(R.id.max_temp).text = tempMax
+                findViewById<TextView>(R.id.min_temp).text = tempMin
+                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.wind).text = windSpeed
+                findViewById<TextView>(R.id.humidity).text = humidity
+
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
             }
             catch (e: java.lang.Exception) {
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
